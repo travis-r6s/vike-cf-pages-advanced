3b9da24d5e3082ca3920ff209706905733308c82
Travis Reynolds <travis@travisreynolds.dev>
2023-12-18 23:55:42 +0000

    test removing faker

--- apps/demo-app/functions/api/posts/index.ts
+++ apps/demo-app/functions/api/posts/index.ts
@@ -1,14 +1,14 @@
-import { faker } from '@faker-js/faker'
+// import { faker } from '@faker-js/faker'
 import type { Function } from '../../utils'
 import { createResponse } from '../../utils'
 
 export const onRequest: Function = (ctx) => {
   const { respond } = createResponse(ctx)
-  const posts = Array.from({ length: faker.number.int({ min: 10, max: 50 }) }).map(() => ({
-    id: faker.string.nanoid(),
-    title: faker.lorem.sentence(),
-    image: faker.image.url(),
-    content: faker.lorem.paragraphs({ min: 2, max: 8 }),
+  const posts = Array.from({ length: 20 }).map(() => ({
+    id: 'faker.string.nanoid()',
+    title: 'faker.lorem.sentence()',
+    image: 'faker.image.url()',
+    content: 'faker.lorem.paragraphs({ min: 2, max: 8 })',
   }))
 
   return respond(200, posts)
