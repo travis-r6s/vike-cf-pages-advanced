72479f22a368e64bcbe828ec200e9805124538b9
Travis Reynolds <travis@travisreynolds.dev>
2023-12-19 23:14:52 +0000

    Update to use custom env file

--- apps/demo-app/vite.config.ts
+++ apps/demo-app/vite.config.ts
@@ -4,6 +4,11 @@ import Vike from 'vike/plugin'
 import type { UserConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
+import dotenv from 'dotenv'
+
+// We are using the Workers .dev.vars env file, to keep things simple and have a single env file.
+dotenv.config({ path: './.dev.vars' })
+
 const config: UserConfig = {
   server: {
     port: 3000
