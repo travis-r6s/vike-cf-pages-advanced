966cf2aebc88532c8a384e4395e6eed75dccf257
Travis Reynolds <travis@travisreynolds.dev>
2023-12-20 11:42:20 +0000

    Add styling page + note

---
+++ apps/demo-app/pages/styling/+Page.tsx
@@ -0,0 +1,11 @@
+// TODO: Update to explain the styling this projects uses - how it uses SCSS, and imports components
+// Also note this is a demo, so they would likely use styling differently.
+
+export function Page() {
+  return (
+    <div className="page">
+      <PageHeader title="Styling" />
+      <p className="tp-body-1">Example of using Vike.</p>
+    </div>
+  )
+}
