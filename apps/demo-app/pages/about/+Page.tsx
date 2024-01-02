e8c11a16bb8b8d71fff5305a6001ef1a4c05c04a
Travis Reynolds <travis@travisreynolds.dev>
2024-01-02 18:43:23 +0000

    Simple about update

--- apps/demo-app/pages/about/+Page.tsx
+++ apps/demo-app/pages/about/+Page.tsx
@@ -1,10 +1,9 @@
-// TODO: Update to explain this project, what it is trying to show, how it is built, credits.
-
 export function Page() {
   return (
     <div className="page">
       <PageHeader title="About" />
-      <p className="tp-body-1">Example of using Vike.</p>
+      <p className="tp-body-1">This example was created by [Travis Reynolds](https://github.com/travis-r6s), and sponsored by: </p>
+      <p className="tp-body-1">It was created to show how an SSR project could be built with Vike and hosted on Cloudflare Pages, and make use of the edge platform to create a website and API's.</p>
     </div>
   )
 }
